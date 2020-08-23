51b2f8f3e1820fea565241216c387763ade66b31
michael watson <mwatson10@gmail.com>
2020-08-23 18:15:58 -0500

    just tests

--- qiskit/test.py
+++ qiskit/test.py
@@ -10,3 +10,10 @@ with open('out.txt','w',encoding='utf8') as f:
     f.write('α = {}'.format(α))
     f.write('\n')
     f.write('β = {}'.format(β))
+
+
+import numpy as np
+np.array[[0,2,1],
+        [2,2,3]]
+
+        
